26306dd7e2c03e987a63aa38f0e9677d6184a160
Anindita1310 <165119244+Anindita1310@users.noreply.github.com>
2024-08-02 13:06:52 +0530

    Create main.java

---
+++ dynamicProgramming/main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class main
+{
+    static int counter=0;
+    public static int fib(int n)
+    {
+        counter++;
+        if(n==0||n==1)
+        return n;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[]args)
+    {
+        int n=20;
+        System.out.println(fib(n));
+        System.out.println("No of iterations  "+counter);
+    }
+}
