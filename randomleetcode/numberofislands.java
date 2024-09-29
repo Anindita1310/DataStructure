1e697f2d7007eacfb6c875cc7404e102caa6a1b4
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2024-09-29 15:12:42 +0530

    Create numberofislands.java

---
+++ randomleetcode/numberofislands.java
@@ -0,0 +1,40 @@
+public class numberofislands {
+    public int numIslands(char[][]grid){
+        if(grid==null||grid.length==0){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][]grid,int i,int j){
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]!='1'){
+            return;
+        }
+        grid[i][j]='#';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+    public static void main(String[] args) {
+        numberofislands obj = new numberofislands();
+        char[][] grid = {
+            {'1', '1', '1', '1', '0'},
+            {'1', '1', '0', '1', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'}
+        };
+        System.out.println("Number of islands: " + obj.numIslands(grid));
+
+        
+    }
+    
+}
